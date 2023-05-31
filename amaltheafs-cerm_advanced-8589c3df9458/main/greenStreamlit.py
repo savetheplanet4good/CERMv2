22f317fa2da1f15c8e00c0636d8dd015126a2596
Haylee Anderson <hayleealyssalyn.anderson@finastra.com>
2023-05-31 00:18:45 -0400

    changed paramaters

--- amaltheafs-cerm_advanced-8589c3df9458/main/greenStreamlit.py
+++ amaltheafs-cerm_advanced-8589c3df9458/main/greenStreamlit.py
@@ -74,14 +74,14 @@ def params():
     p = st.sidebar.slider( 'Physical Risk Accelaration ', min_value=0, step=1, max_value=30, value=10, format="%d%%")
     st.sidebar.markdown( """ 
                             <p style= font-size:9px;color:#898A8B;margin-top:-100px;margin-left:165px;'>
-                            '%' of economic risk standard deviation
+                            % of economic risk standard deviation
                             </p>
                         """, unsafe_allow_html=True
                         )
-    stress_test = st.sidebar.slider( 'Physical Drift', min_value=-100, step=1, max_value=0, value=-50, format="%d%%")
+    stress_test = st.sidebar.slider( 'Physical Risk Drift', min_value=-100, step=1, max_value=0, value=-50, format="%d%%")
     st.sidebar.markdown( """ 
-                            <p style= font-size:11px;color:#898A8B;margin-top:-100px;margin-left:95px;'>
-                            '%' of physical risk standard deviation
+                            <p style= font-size:11px;color:#898A8B;margin-top:-100px;margin-left:97px;'>
+                            % of physical risk standard deviation
                             </p>
                         """, unsafe_allow_html=True
                         )
