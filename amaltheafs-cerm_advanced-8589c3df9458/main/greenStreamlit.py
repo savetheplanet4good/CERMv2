5417d69eae8aaef79fafafdec7164b8e0e49a139
Haylee Anderson <hayleealyssalyn.anderson@finastra.com>
2023-05-31 00:20:20 -0400

    changed paramatersss

--- amaltheafs-cerm_advanced-8589c3df9458/main/greenStreamlit.py
+++ amaltheafs-cerm_advanced-8589c3df9458/main/greenStreamlit.py
@@ -80,7 +80,7 @@ def params():
                         )
     stress_test = st.sidebar.slider( 'Physical Risk Drift', min_value=-100, step=1, max_value=0, value=-50, format="%d%%")
     st.sidebar.markdown( """ 
-                            <p style= font-size:11px;color:#898A8B;margin-top:-100px;margin-left:100px;'>
+                            <p style= font-size:11px;color:#898A8B;margin-top:-100px;margin-left:108px;'>
                             % of physical risk standard deviation
                             </p>
                         """, unsafe_allow_html=True
